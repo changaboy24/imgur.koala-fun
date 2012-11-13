17099bd0fded8ed07de0176de9a6f0e13b910f63
rich-chang <changaboy24@gmail.com>
2012-11-13 01:21:52 -0600

    resized imgur to thumbnails

--- api.js
+++ api.js
@@ -1,6 +1,3 @@
-    /* Drag'n drop stuff */
-    window.ondragover = function(e) {e.preventDefault()}
-    window.ondrop = function(e) {e.preventDefault(); upload(e.dataTransfer.files[0]); }
     function upload(file) {
 
         /* Is the file an image? */
@@ -15,6 +12,7 @@
         fd.append("key", "6528448c258cff474ca9701c5bab6927"); // Get your own key http://api.imgur.com/
         var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
         xhr.open("POST", "http://api.imgur.com/2/upload.json"); // Boooom!
+
         xhr.onload = function() {
             //imgur API gives back imgur.com/3asds...
             //need to get specific jpg
@@ -22,12 +20,11 @@
             $('#imgur').empty();
             $('#koala').empty();
             var url = JSON.parse(xhr.responseText).upload.links.imgur_page;
-            url = url.insert(7,"i.")+".jpg";
+            url = url.insert(7,"i.")+"b.jpg";
             var koala_url = "http://www.koalastothemax.com/?"+url;
 
             var img = document.createElement("img");
             img.src = url;
-            //$('imgur').appendChild(img);
             var imgur = document.getElementById('imgur');
             imgur.appendChild(img);
 
